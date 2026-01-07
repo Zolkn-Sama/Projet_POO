321fc595b555e324b0e5cb01ba35ad42c12ba2f6
Zolkn-sama <enzo.land01630@gmail.com>
2026-01-07 17:33:52 +0100

    first commit

---
+++ src/main/java/Projet_POO/Repository/UserRepository.java
@@ -0,0 +1,5 @@
+package Projet_POO.Repository;
+
+public class UserRepository {
+    
+}
---
+++ src/test/java/Projet_POO/ProjetPooApplicationTests.java
@@ -0,0 +1,13 @@
+package Projet_POO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetPooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
