653719ec885cd875bcb6a1a098825bc3c2ccab24
meriemmerry <meriemmerry461@gmail.com>
2026-01-08 15:29:37 +0100

    Update ProjetPooApplication.java

--- src/main/java/Projet_POO/ProjetPooApplication.java
+++ src/main/java/Projet_POO/ProjetPooApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EntityScan(basePackages = "Projet_POO.Domain.Entity")
+@EnableJpaRepositories(basePackages = "Projet_POO.Repository")
 public class ProjetPooApplication {
 
 	public static void main(String[] args) {
