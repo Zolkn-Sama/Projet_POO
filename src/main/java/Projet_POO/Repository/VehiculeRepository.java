a4d024095d61a7a48a0268edd38a50260865867d
meriemmerry <meriemmerry461@gmail.com>
2026-01-08 14:43:02 +0100

    VehiculeRepository

---
+++ src/main/java/Projet_POO/Repository/VehiculeRepository.java
@@ -0,0 +1,12 @@
+package Projet_POO.Repository;
+
+
+import Projet_POO.Domain.Entity.Vehicule;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import java.util.Optional;
+
+public interface VehiculeRepository extends JpaRepository<Vehicule, Long> {
+    Optional<Vehicule> findByImmatriculation(String immatriculation);
+    List<Vehicule> findByVilleDisponibiliteIgnoreCase(String ville);
+}
