f679d788244673c3ed724b88f437e784b92df841
sami-K2R <samicarrere25@gmail.com>
2026-01-12 23:44:07 +0100

    Part OptionVehicule

--- src/main/java/Projet_POO/Repository/OptionVehiculeRepository.java
+++ src/main/java/Projet_POO/Repository/OptionVehiculeRepository.java
@@ -1,7 +1,15 @@
 package Projet_POO.Repository;
 
-import Projet_POO.Domain.Entity.OptionVehicule;
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import Projet_POO.Domain.Entity.OptionVehicule;
+import Projet_POO.Domain.Enums.CodeOption;
+
+@Repository
 public interface OptionVehiculeRepository extends JpaRepository<OptionVehicule, Long> {
+
+    Optional<OptionVehicule> findByCode(CodeOption code);
 }
