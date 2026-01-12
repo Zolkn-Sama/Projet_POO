a3f2ce59edc5e619191d274ec7b95c14096f0796
sami-K2R <samicarrere25@gmail.com>
2026-01-12 21:35:45 +0100

    Part CaracteristiquesVehicule

--- src/main/java/Projet_POO/Controller/CaracteristiquesVehiculeController.java
+++ src/main/java/Projet_POO/Controller/CaracteristiquesVehiculeController.java
@@ -1,33 +1,69 @@
 package Projet_POO.Controller;
 
-import Projet_POO.Domain.Entity.CaracteristiquesVehicule;
-import Projet_POO.Service.CaracteristiquesVehiculeService;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import Projet_POO.Domain.Entity.CaracteristiquesVehicule;
+import Projet_POO.Service.CaracteristiquesVehiculeService;
 
 @RestController
 @RequestMapping("/caracteristiques-vehicules")
 public class CaracteristiquesVehiculeController {
 
-    private final CaracteristiquesVehiculeService service;
+    private final CaracteristiquesVehiculeService caracteristiquesVehiculeService;
 
-    public CaracteristiquesVehiculeController(CaracteristiquesVehiculeService service) {
-        this.service = service;
-    }
-
-    @PostMapping
-    public CaracteristiquesVehicule creer(@RequestBody CaracteristiquesVehicule caracteristiquesVehicule) {
-        return service.creer(caracteristiquesVehicule);
+    public CaracteristiquesVehiculeController(CaracteristiquesVehiculeService caracteristiquesVehiculeService) {
+        this.caracteristiquesVehiculeService = caracteristiquesVehiculeService;
     }
 
     @GetMapping
-    public List<CaracteristiquesVehicule> toutes() {
-        return service.toutes();
+    public List<CaracteristiquesVehicule> getAll() {
+        return caracteristiquesVehiculeService.findAll();
     }
 
     @GetMapping("/{id}")
-    public CaracteristiquesVehicule trouverParId(@PathVariable Long id) {
-        return service.trouverParId(id);
+    public CaracteristiquesVehicule getById(@PathVariable Long id) {
+        return caracteristiquesVehiculeService.findById(id);
+    }
+
+    @GetMapping("/marque/{marque}")
+    public List<CaracteristiquesVehicule> getByMarque(@PathVariable String marque) {
+        return caracteristiquesVehiculeService.findByMarque(marque);
+    }
+
+    @GetMapping("/modele/{modele}")
+    public List<CaracteristiquesVehicule> getByModele(@PathVariable String modele) {
+        return caracteristiquesVehiculeService.findByModele(modele);
+    }
+
+    @GetMapping("/couleur/{couleur}")
+    public List<CaracteristiquesVehicule> getByCouleur(@PathVariable String couleur) {
+        return caracteristiquesVehiculeService.findByCouleur(couleur);
+    }
+
+    @GetMapping("/permis/{categoriePermisRequise}")
+    public List<CaracteristiquesVehicule> getByPermis(@PathVariable String categoriePermisRequise) {
+        return caracteristiquesVehiculeService.findByCategoriePermisRequise(categoriePermisRequise);
+    }
+
+    @GetMapping("/places/{nbPlaces}")
+    public List<CaracteristiquesVehicule> getByNbPlaces(@PathVariable int nbPlaces) {
+        return caracteristiquesVehiculeService.findByNbPlaces(nbPlaces);
+    }
+
+    @PostMapping
+    public CaracteristiquesVehicule create(@RequestBody CaracteristiquesVehicule caracteristiquesVehicule) {
+        return caracteristiquesVehiculeService.create(caracteristiquesVehicule);
+    }
+
+    @PutMapping("/{id}")
+    public CaracteristiquesVehicule update(@PathVariable Long id, @RequestBody CaracteristiquesVehicule caracteristiquesVehicule) {
+        return caracteristiquesVehiculeService.update(id, caracteristiquesVehicule);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        caracteristiquesVehiculeService.delete(id);
     }
 }
