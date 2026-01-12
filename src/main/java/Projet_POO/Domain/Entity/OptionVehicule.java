ce61f0336ff8f618689807e9041b0fd3e8c7b003
Zolkn-sama <enzo.land01630@gmail.com>
2026-01-12 14:30:52 +0100

    Correctif Enzo

--- src/main/java/Projet_POO/Domain/Entity/OptionVehicule.java
+++ src/main/java/Projet_POO/Domain/Entity/OptionVehicule.java
@@ -1,7 +1,14 @@
 package Projet_POO.Domain.Entity;
 
-import Projet_POO.Domain.Enums.NomOption;
-import jakarta.persistence.*;
+import Projet_POO.Domain.Enums.CodeOption;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "option_vehicule")
@@ -13,18 +20,18 @@ public class OptionVehicule {
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false, unique = true)
-    private NomOption nom;
+    private CodeOption nom;
 
     public OptionVehicule() {}
 
-    public OptionVehicule(NomOption nom) {
+    public OptionVehicule(CodeOption nom) {
         this.nom = nom;
     }
 
     public Long getId() { return id; }
 
-    public NomOption getNom() { return nom; }
-    public void setNom(NomOption nom) { this.nom = nom; }
+    public CodeOption getNom() { return nom; }
+    public void setNom(CodeOption nom) { this.nom = nom; }
 
     @Override
     public String toString() {
