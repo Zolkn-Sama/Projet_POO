382be5635a0d66374d3697ecce961babdffcad52
sami-K2R <samicarrere25@gmail.com>
2026-01-14 21:37:01 +0100

    Debug V2

--- src/main/java/Projet_POO/Domain/Entity/Vehicule.java
+++ src/main/java/Projet_POO/Domain/Entity/Vehicule.java
@@ -20,7 +20,6 @@ public class Vehicule {
     private String immatriculation;
     private Localisation localisationVehicule;
     private boolean deposeDifferenteAutorisee;
-    private Long agentId;
 
 
     private double prixJournalier;
@@ -72,8 +71,6 @@ public class Vehicule {
         this.deposeDifferenteAutorisee = deposeDifferenteAutorisee;
         this.typeVehicule = typeVehicule;
     }
-    public Long getAgentId() { return agentId; }
-    public void setAgentId(Long agentId) { this.agentId = agentId; }
 
     // --- Getters / Setters ---
     public Long getId() {
