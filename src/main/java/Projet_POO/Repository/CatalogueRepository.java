418cf830b7e4b7e2da75b3f6c607addc4fdfabbc
meriemmerry <meriemmerry461@gmail.com>
2026-01-19 11:53:20 +0100

    Update CatalogueRepository.java

--- src/main/java/Projet_POO/Repository/CatalogueRepository.java
+++ src/main/java/Projet_POO/Repository/CatalogueRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CatalogueRepository extends JpaRepository<Vehicule, Long> {
-    // Le nom ici DOIT être CatalogueRepository
+    
 }
