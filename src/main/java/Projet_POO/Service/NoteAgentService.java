249b8ecde269fcc1d3da8fa6e400c1cec1ecf149
meriemmerry <meriemmerry461@gmail.com>
2026-01-19 13:18:13 +0100

    Update NoteAgentService.java

--- src/main/java/Projet_POO/Service/NoteAgentService.java
+++ src/main/java/Projet_POO/Service/NoteAgentService.java
@@ -4,7 +4,6 @@ import Projet_POO.Domain.Entity.NoteAgent;
 import java.util.List;
 
 public interface NoteAgentService {
-    // 🟢 Mise à jour de la signature : ajout de l'ID de l'agent
     NoteAgent creer(NoteAgent note, Long agentId);
 
     List<NoteAgent> toutes();
