3f449c0c5e216765a3455698f9da859a3f40bad1
meriemmerry <meriemmerry461@gmail.com>
2026-01-19 11:20:35 +0100

    Update NoteLoueurController.java

--- src/main/java/Projet_POO/Controller/NoteLoueurController.java
+++ src/main/java/Projet_POO/Controller/NoteLoueurController.java
@@ -15,29 +15,24 @@ public class NoteLoueurController {
     @Autowired
     private NoteLoueurService noteLoueurService;
 
-    /**
-     * Enregistre une évaluation pour un Loueur spécifique.
-     * URL : POST /api/notes-loueur/{loueurId}
-     */
-    @PostMapping("/{loueurId}") // 🟢 CHANGEMENT : L'ID est dans l'URL
+    // Enregistre une évaluation pour un Loueur spécifique
+
+    @PostMapping("/{loueurId}") 
     @ResponseStatus(HttpStatus.CREATED)
     public NoteLoueur create(@PathVariable Long loueurId, @RequestBody NoteLoueur note) {
-        // On passe l'ID récupéré dans l'URL + le contenu de la note (JSON) au service
+        // On passe le contenu de la note (JSON) au service
         return noteLoueurService.creer(note, loueurId);
     }
 
-    /**
-     * Liste toutes les évaluations de tous les loueurs (Optionnel).
-     */
+    // Liste toutes les évaluations de tous les loueurs (Optionnel).
+
     @GetMapping
     public List<NoteLoueur> getAll() {
         return noteLoueurService.toutes();
     }
 
-    /**
-     * Récupère la liste des notes d'un loueur spécifique par son identifiant.
-     * URL : GET /api/notes-loueur/loueur/{loueurId}
-     */
+    // Récupère la liste des notes d'un loueur spécifique par son identifiant.
+
     @GetMapping("/loueur/{loueurId}")
     public List<NoteLoueur> getByLoueur(@PathVariable Long loueurId) {
         return noteLoueurService.parLoueur(loueurId);
