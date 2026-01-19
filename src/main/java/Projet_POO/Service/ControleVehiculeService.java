733b323b293ba6fcd94326b267288d5e25645c67
meriemmerry <meriemmerry461@gmail.com>
2026-01-19 13:17:29 +0100

    Update ControleVehiculeService.java

--- src/main/java/Projet_POO/Service/ControleVehiculeService.java
+++ src/main/java/Projet_POO/Service/ControleVehiculeService.java
@@ -4,9 +4,6 @@ import Projet_POO.Domain.Entity.ControleVehicule;
 import java.util.List;
 
 public interface ControleVehiculeService {
-    // Crée une fiche de contrôle liée à un véhicule spécifique
     ControleVehicule creer(ControleVehicule controle, Long vehiculeId);
-
-    // Génère les alertes en cherchant la fiche par l'ID du véhicule
     List<String> genererAlertesPourVehicule(Long vehiculeId);
 }
